4b73cca0e531debd5f127f7952011d02096b4ee6
dineshkannaak <dineshkannaa.s@gmail.com>
2026-07-14 08:50:51 +0530

    Update app .py

--- app .py
+++ app .py
@@ -69,7 +69,8 @@ def load_pipeline(api_key, pdf_path, file_hash):
     llm = ChatGroq(
         model="llama-3.1-8b-instant",
         api_key=api_key,
-        temperature=0.0
+        temperature=0.0,
+        max_tokens=600  
     )
 
     loader = PyPDFLoader(pdf_path)
